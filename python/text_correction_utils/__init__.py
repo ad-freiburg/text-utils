f3951e87bb7a182b499789280807c48bf70ab72b
Sebastian Walter <sebastian.walter98@gmail.com>
2023-02-02 19:06:47 +0100

    udpate

--- python/text_correction_utils/__init__.py
+++ python/text_correction_utils/__init__.py
@@ -1,13 +1,5 @@
-from text_correction_utils import (
-    api,
-    inference,
-    modules,
-    logging,
-    distributed,
-    mask,
-    io
-)
-from text_correction_utils._internal import (
+from text_correction_utils.version import __version__
+from ._internal import (
     edit,
     text,
     whitespace,
@@ -18,7 +10,15 @@ from text_correction_utils._internal import (
     metrics,
     unicode
 )
-from text_correction_utils.version import __version__
+from text_correction_utils import (
+    api,
+    inference,
+    modules,
+    logging,
+    distributed,
+    mask,
+    io
+)
 import sys
 import os
 sys.path.append(os.path.dirname(__file__))

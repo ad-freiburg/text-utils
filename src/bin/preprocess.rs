b9165e9f89e43d93bf7f9b26d0d1c61c22d48029
Sebastian Walter <sebastian.walter98@gmail.com>
2023-05-03 11:39:22 +0200

    upload preprocess bin

---
+++ src/bin/preprocess.rs
@@ -0,0 +1,170 @@
+use std::{
+    path::PathBuf,
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
+use text_correction_utils::{
+    data::{
+        loading::{
+            text_data_generator_from_files, BatchLimitType, BatchedIterator, BufferedIterator,
+            PipelineIterator, TensorizedIterator, TextIterationStrategy, TextIterator,
+        },
+        preprocessing::{LabelingConfig, MaskMode, PreprocessingFnConfig, SpellingCorruptionMode},
+        text_data_pipeline_with_tokenizer, PreprocessingConfig, PreprocessingPipelineConfig,
+        TextDataInfo,
+    },
+    text::file_size,
+    tokenization::{
+        ByteGroups, ByteTokenizerConfig, GroupAggregation, SpecialConfig, TokenizeConfig,
+        TokenizerConfig, BOS, EOS, PAD,
+    },
+    unicode::Normalization,
+    utils::progress_bar,
+};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, num_args(1..))]
+    files: Vec<String>,
+
+    #[arg(short, long, default_value_t = 4)]
+    num_threads: u8,
+
+    #[arg(short, long, default_value_t = 65536)]
+    batch_limit: usize,
+
+    #[arg(short, long, default_value_t = 10000)]
+    limit: usize,
+
+    #[arg(short, long, default_value_t = 32)]
+    buffer_size: usize,
+
+    #[arg(short, long, default_value_t = 32)]
+    prefetch_factor: usize,
+
+    #[arg(short, long, default_value_t = 10)]
+    delay_ms: u64,
+
+    #[arg(short, long)]
+    char_file: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    assert!(
+        !args.files.is_empty(),
+        "expected at least one file for preprocessing"
+    );
+
+    let tokenizer_cfg = TokenizerConfig {
+        tokenize: TokenizeConfig::Byte(ByteTokenizerConfig {
+            groups: ByteGroups::CodePoints,
+            aggregation: GroupAggregation::Mean,
+            use_graphemes: true,
+            pad_to_multiple_of: Some(32),
+        }),
+        special: SpecialConfig {
+            pad: PAD.into(),
+            prefix: vec![BOS.into()],
+            suffix: vec![EOS.into()],
+            tokens: vec![BOS.into(), EOS.into(), PAD.into()],
+        },
+        language: None,
+    };
+    let pipeline_cfg = PreprocessingPipelineConfig {
+        preprocessing: PreprocessingConfig::Single(vec![
+            PreprocessingFnConfig::Clean(true),
+            PreprocessingFnConfig::Normalize(Normalization::NFKC, true),
+            PreprocessingFnConfig::ByteSubstring(512, true),
+            PreprocessingFnConfig::SpellingCorruption(
+                0.2,
+                true,
+                SpellingCorruptionMode::Artificial(0.25, 2.0, args.char_file),
+            ),
+            PreprocessingFnConfig::WhitespaceCorruption(0.05, 0.2, true),
+            PreprocessingFnConfig::MaskCorruption(
+                0.2,
+                true,
+                "<mask>".to_string(),
+                MaskMode::Char(3, 0.2),
+            ),
+        ]),
+        labeling: LabelingConfig::SequenceGeneration(tokenizer_cfg.clone()),
+    };
+    let pipeline = text_data_pipeline_with_tokenizer(pipeline_cfg, tokenizer_cfg.clone())?;
+
+    let seed = 22;
+    let num_lines: usize = args
+        .files
+        .iter()
+        .filter_map(|f| file_size(f).ok())
+        .map(|(lines, _)| lines)
+        .sum();
+    let generators = args
+        .files
+        .iter()
+        .map(|f| text_data_generator_from_files(f, None, None))
+        .collect::<anyhow::Result<_>>()?;
+    let text_iter = TextIterator::new(generators, TextIterationStrategy::Weighted, Some(seed))?;
+
+    let mut iter = text_iter
+        .enumerate()
+        .filter_map(move |(item_idx, (d, file_idx))| {
+            if let Ok(d) = d {
+                Some((
+                    d,
+                    TextDataInfo {
+                        file_idx,
+                        seed: seed + item_idx as u64,
+                    },
+                ))
+            } else {
+                None
+            }
+        })
+        .take(args.limit)
+        .pipe(pipeline, args.num_threads)
+        .filter_map(|i| i.ok())
+        .batched(
+            true,
+            true,
+            args.prefetch_factor,
+            args.batch_limit,
+            BatchLimitType::PaddedItemSize,
+            Some(seed),
+        )
+        .tensorized(tokenizer_cfg)?
+        .buffered(args.buffer_size);
+
+    let start = Instant::now();
+    let mut load_times = vec![];
+    let mut sizes = vec![];
+    let p_bar = progress_bar("preprocessing", num_lines.min(args.limit) as u64, false);
+    loop {
+        let load_batch = Instant::now();
+        if let Some(item) = iter.next() {
+            load_times.push(load_batch.elapsed().as_secs_f64());
+            sizes.push(item.0.len());
+            p_bar.inc(item.0.len() as u64);
+        } else {
+            break;
+        };
+        // simulates model running
+        sleep(Duration::from_millis(args.delay_ms));
+    }
+    p_bar.finish_and_clear();
+    println!(
+        "average batch load time: {:.2}ms",
+        1000.0 * load_times.iter().sum::<f64>() / load_times.len() as f64
+    );
+    println!(
+        "average batch size: {:.2}",
+        sizes.iter().sum::<usize>() as f64 / sizes.len() as f64
+    );
+    println!("preprocessing took {:.2}s", start.elapsed().as_secs_f64());
+
+    Ok(())
+}

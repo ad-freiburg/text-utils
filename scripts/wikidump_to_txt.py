7c015a9d6da033109d8c5ff22bb7326595d2ebb3
Sebastian Walter <sebastian.walter98@gmail.com>
2023-05-04 17:17:09 +0200

    update wikidump script

--- scripts/wikidump_to_txt.py
+++ scripts/wikidump_to_txt.py
@@ -6,10 +6,14 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--files", type=str, required=True, nargs="+", help="Files to process")
     parser.add_argument("-o", "--output", type=str, required=True, help="Output file")
+    parser.add_argument("--min-chars", type=int, default=10)
+    parser.add_argument("--min-words", type=int, default=2)
     return parser.parse_args()
 
 
 def run(args: argparse.Namespace):
+    total = 0
+    valid = 0
     with open(args.output, "w", encoding="utf8") as of:
         for file in tqdm(args.files, desc="Processing files", leave=False):
             with open(file, "r", encoding="utf8") as inf:
@@ -17,7 +21,15 @@ def run(args: argparse.Namespace):
                     line = line.strip()
                     if line.startswith("<doc id=") or line.startswith("</doc>") or line == "":
                         continue
+                    total += 1
+                    has_chars = len(line) >= args.min_chars
+                    has_words = len(line.split()) >= args.min_words
+                    if not has_chars and not has_words:
+                        continue
                     of.write(line + "\n")
+                    valid += 1
+    # print percentage of valid lines
+    print(f"valid lines: {valid}/{total} ({100 * valid / total:.2f}%)")
 
 
 if __name__ == "__main__":

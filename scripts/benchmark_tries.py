378f038f309abd74164c4ba0edc2acdf7e769a40
Sebastian Walter <sebastian.walter98@gmail.com>
2023-05-31 20:30:34 +0200

    update benchmark script

--- scripts/benchmark_tries.py
+++ scripts/benchmark_tries.py
@@ -16,11 +16,15 @@ def parse_args() -> argparse.Namespace:
 
 def benchmark(args: argparse.Namespace):
     pairs = []
-    with open(args.file) as f:
-        for line in f:
+    with open(args.file, "r", encoding="utf8") as f:
+        for i, line in enumerate(f):
             line = line.strip()
             split = line.split("\t")
-            pairs.append((split[0], split[1]))
+            try:
+                pairs.append((split[0], split[1]))
+            except IndexError:
+                print(f"error in line {i}:\n{line}\n{split}")
+                exit(1)
 
     if args.limit is not None:
         pairs = pairs[:args.limit]
@@ -41,25 +45,25 @@ def benchmark(args: argparse.Namespace):
 
     start = time.perf_counter()
     trie = prefix_tree.Tree()
-    for property, name in tqdm(pairs, "creating prefix tree", leave=False):
-        trie.insert(name, property)
+    for name, identifier in tqdm(pairs, "creating prefix tree", leave=False):
+        trie.insert(name, int(identifier[1:]))
     end = time.perf_counter()
     print(f"Rust prefix tree: Built in {end - start:.2f} seconds")
 
     start = time.perf_counter()
-    for property, name in tqdm(pairs, "getting values", leave=False):
+    for name, _ in tqdm(pairs, "getting values", leave=False):
         trie.get(name)
     end = time.perf_counter()
     print(f"Rust prefix tree: Get value in {1000 * (end - start):.2f}ms")
 
     start = time.perf_counter()
-    for property, name in tqdm(pairs, "checking prefixes", leave=False):
+    for name, _ in tqdm(pairs, "checking prefixes", leave=False):
         trie.contains_prefix(name)
     end = time.perf_counter()
     print(f"Rust prefix tree: Check for prefix in {1000 * (end - start):.2f}ms")
 
     start = time.perf_counter()
-    for property, name in tqdm(pairs[:n], "checking continuations", leave=False):
+    for name, _ in tqdm(pairs[:n], "checking continuations", leave=False):
         for cont in continuations:
             trie.contains_prefix(name + cont)
     end = time.perf_counter()
@@ -68,7 +72,7 @@ def benchmark(args: argparse.Namespace):
           f"({runtime / min(len(pairs), n):.2f}ms on avg)")
 
     start = time.perf_counter()
-    for property, name in tqdm(pairs[:n], "checking continuations", leave=False):
+    for name, _ in tqdm(pairs[:n], "checking continuations", leave=False):
         trie.contains_continuations(name, continuations)
     end = time.perf_counter()
     runtime = 1000 * (end - start)
@@ -77,19 +81,19 @@ def benchmark(args: argparse.Namespace):
 
     start = time.perf_counter()
     trie = marisa_trie.BytesTrie(
-        ((name, property.encode())
-         for property, name in pairs)
+        ((name, identifier.encode())
+         for name, identifier in pairs)
     )
     end = time.perf_counter()
     print(f"Marisa trie: Built in {end - start:.2f} seconds")
     start = time.perf_counter()
-    for property, name in pairs:
+    for name, _ in pairs:
         trie.get(name)[0]
     end = time.perf_counter()
     print(f"Marisa trie: Get value in {1000 * (end - start):.2f}ms")
 
     start = time.perf_counter()
-    for property, name in pairs:
+    for name, _ in pairs:
         trie.keys(name)
     end = time.perf_counter()
     print(f"Marisa trie: Check for prefix in {1000 * (end - start):.2f}ms")
